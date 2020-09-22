43a2e1205a8dfae4ac0a0f61ab0725d89cdac7d9
Emmanuel Tweneboah <macbook@MacBooks-MacBook-Pro.local>
2020-09-22 15:29:29 +0800

    dev2

---
+++ config/mongodbConnect.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const keys = require('./myKeys');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(keys.mongoURI, {
+      useFindAndModify: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log('DB Connected....😎');
+  } catch (error) {
+    console.log(error.message);
+    //exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
