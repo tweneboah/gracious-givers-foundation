09e38f2f16b079192fd71e294c5f36eef8897cdb
Emmanuel Tweneboah <macbook@MacBooks-MacBook-Pro.local>
2020-09-22 14:57:55 +0800

    dev2

---
+++ config/myKeys.js
@@ -0,0 +1,17 @@
+// module.exports = {
+//   googleClientID:
+//     '548271998197-gq4cudd9iqmagj3l6rhndl2ugc8u28be.apps.googleusercontent.com',
+//   googleClientSecret: 'vpxHIyOdXRmJdASE-KEss9HX',
+//   mongoURI: 'mongodb://127.0.0.1:27017/gracious-givers',
+//   cookieKeys: '4dssfdfdf33342sdsdsdsd',
+// };
+
+const myKeys = {
+  googleClientID:
+    '548271998197-gq4cudd9iqmagj3l6rhndl2ugc8u28be.apps.googleusercontent.com',
+  googleClientSecret: 'vpxHIyOdXRmJdASE-KEss9HX',
+  mongoURI: 'mongodb://127.0.0.1:27017/gracious-givers',
+  cookieKeys: '4dssfdfdf33342sdsdsdsd',
+};
+
+module.exports = myKeys;
