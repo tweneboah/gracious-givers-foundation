53d0e2ef964468f76bd6539228494826753ce289
Emmanuel Tweneboah <macbook@MacBooks-MacBook-Pro.local>
2020-09-22 16:18:35 +0800

    DEV

--- config/keys.js
+++ config/keys.js
@@ -10,7 +10,9 @@ const keys = {
   googleClientID:
     '548271998197-gq4cudd9iqmagj3l6rhndl2ugc8u28be.apps.googleusercontent.com',
   googleClientSecret: 'vpxHIyOdXRmJdASE-KEss9HX',
-  mongoURI: 'mongodb://127.0.0.1:27017/gracious-givers',
+  mongoURILOCAL: 'mongodb://127.0.0.1:27017/gracious-givers',
+  mongoURI:
+    '//mongodb+srv://emmanuel:dhzK8zyKAavE6DFl@cluster0.lucy4.mongodb.net/gracious-givers-foundation?retryWrites=true&w=majority',
   cookieKeys: '4dssfdfdf33342sdsdsdsd',
 };
 
